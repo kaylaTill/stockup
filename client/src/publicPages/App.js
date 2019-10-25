e9360c9ae8e2425619623dae2415598eec9ce0ec
kaylaTill <kaylatillman319@gmail.com>
2019-10-25 04:23:39 -0700

    Refactored App to class component

--- client/src/publicPages/App.js
+++ client/src/publicPages/App.js
@@ -6,25 +6,34 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import Logo from '../stockUP-logo-white.png';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-        <Link style={{ textDecoration: 'none'}} to={'/'}>
-          <h2 className="header">
-            StockUP.
-          </h2>
-        </Link>
-        <Register/>
-        
-        <Suspense fallback={<div/>}>
-          <Switch>
-          </Switch>
-        </Suspense>
-      </Router>
-    </div>
-  );
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+  
+          <Link style={{ textDecoration: 'none'}} to={'/'}>
+            <h2 className="header">
+              StockUP.
+            </h2>
+          </Link>
+          <Register/>
+          
+          <Suspense fallback={<div/>}>
+            <Switch>
+            </Switch>
+          </Suspense>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
