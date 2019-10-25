a0b4e3177a2f66e5de8992778c0ae4c735476511
kaylaTill <kaylatillman319@gmail.com>
2019-10-25 02:32:29 -0700

    Added User Login Features

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import PrivatePages from './PrivatePages';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
-render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-), document.getElementById('root'));
+
+const loggedIn = (() => {
+    axios.get('/loggedIn')
+        .then((response) => {
+            ReactDOM.render(
+                <BrowserRouter>
+                    <PrivatePages />
+                </BrowserRouter>,
+                document.getElementById('root')
+            )
+        })
+        .catch((err) => {
+            ReactDOM.render(
+       
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>,
+                document.getElementById('root')
+            );
+        })
+});
+
+loggedIn();
 
 serviceWorker.unregister();
