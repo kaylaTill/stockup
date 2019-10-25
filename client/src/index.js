ecb7b82d29ec007aea7f22a214cf25428931cdb4
kaylaTill <kaylatillman319@gmail.com>
2019-10-25 03:03:32 -0700

    Added Registration and Button Loading on Screen

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './publicPages/App';
 import * as serviceWorker from './serviceWorker';
 import PrivatePages from './privatePages/PrivatePages';
-import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 
 
@@ -12,18 +11,12 @@ const loggedIn = (() => {
     axios.get('/loggedIn')
         .then((response) => {
             ReactDOM.render(
-                <BrowserRouter>
-                    <PrivatePages />
-                </BrowserRouter>,
+                    <PrivatePages />,
                 document.getElementById('root')
             )
         })
         .catch((err) => {
-            ReactDOM.render(
-       
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>,
+            ReactDOM.render( <App />,
                 document.getElementById('root')
             );
         })
--- client/src/publicPages/App.js
+++ client/src/publicPages/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Register from './registration';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      yoohooo
+      <Router>
+        <Button href={'/register'}/>
+
+
+
+        <Suspense fallback={<div/>}>
+          <Switch>
+            <Route exact={true} path={'/'}>
+              <div>Home</div>
+            </Route>
+
+            <Route exact={true} path={'/register'} component={Register}/>
+          </Switch>
+        </Suspense>
+      </Router>
     </div>
   );
 }
