bba7cd78cc433f120890a74d3899e77b0edfb80e
kaylaTill <kaylatillman319@gmail.com>
2019-10-26 17:45:45 -0700

    Added Buy Button

--- client/src/privatePages/trade/Trade.js
+++ client/src/privatePages/trade/Trade.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import StockList from './StockList';
 import { Button } from 'react-bootstrap';
-import Quote from './Quote'
+import Quote from './Quote';
+import Buy from './Buy'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './trade.css';
 
@@ -17,6 +18,8 @@ const Trade = ((props) => {
     return (
         <div>
             <Quote/>
+            <br><br/>
+            <Buy/>
             <StockList/>
         </div>
     )
