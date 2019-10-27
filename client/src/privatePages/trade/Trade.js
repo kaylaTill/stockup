34975d9404ecbe1e398f31c16d27c4d6c3097e83
kaylaTill <kaylatillman319@gmail.com>
2019-10-26 18:18:17 -0700

    API call to IEX working on load

--- client/src/privatePages/trade/Trade.js
+++ client/src/privatePages/trade/Trade.js
@@ -1,29 +1,53 @@
 import React from 'react';
 import StockList from './StockList';
-import { Button } from 'react-bootstrap';
+import axios from 'axios';
 import Quote from './Quote';
 import Buy from './Buy'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './trade.css';
+import API_KEY from '../../key';
+import { symbol } from 'prop-types';
 
-const Trade = ((props) => {
-/*
-    SHOULD HAVE:
-        - SearchBar
-        - Sell Button 
-        - Buy Button 
-        - Quote Button
-        - Render Trade List 
-*/ 
-    return (
-        <div>
-            <Quote/>
-            <Buy/>
-            <StockList/>
-        </div>
-    )
+class Trade extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stocks: []
+        }
+        this.getQuote = this.getQuote.bind(this);
+    }
 
-})
+
+
+    componentDidMount() {
+        this.getQuote('AAPL')
+    }
+
+    getQuote(symbol) {
+        axios.get(`https://cloud.iexapis.com/beta/stock/${symbol}/quote/?token=${API_KEY}&period=annual`)
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+
+    }
+
+
+
+
+    render() {
+        return (
+            <div>
+                <Quote/>
+                <Buy/>
+                <StockList/>
+            </div>
+        )
+    }
+
+}
 
 
 export default Trade;
