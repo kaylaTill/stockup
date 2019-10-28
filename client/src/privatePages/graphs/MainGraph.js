4d8e11e21c420ef04796724433aeccde1c41f37e
kaylaTill <kaylatillman319@gmail.com>
2019-10-28 11:39:59 -0700

    Data set keys setting correctly

--- client/src/privatePages/graphs/MainGraph.js
+++ client/src/privatePages/graphs/MainGraph.js
@@ -8,21 +8,75 @@ class AreaChart extends React.Component {
         super(props);
         
         this.state = {
-            series: [],
-            volume: []
-        }
+            data: [],
+            selection: 'one_year',
+                    options: {
+                        annotations: {
+                            yaxis: [{
+                                y: 30,
+                                borderColor: '#999',
+                            }],
+                            xaxis: [{
+                                x: new Date('14 Nov 2012').getTime(),
+                                borderColor: '#999',
+                                yAxisIndex: 0,
+                            }]
+                        },
+                        dataLabels: {
+                            enabled: false
+                        },
+                        markers: {
+                            size: 0,
+                            style: 'hollow',
+                        },
+                        xaxis: {
+                            type: 'datetime',
+                            min: new Date('01 Mar 2012').getTime(),
+                            tickAmount: 6,
+                        },
+                        tooltip: {
+                            x: {
+                                format: 'dd MMM yyyy'
+                            }
+                        },
+                        fill: {
+                            type: 'gradient',
+                            gradient: {
+                                shadeIntensity: 1,
+                                opacityFrom: 0.7,
+                                opacityTo: 0.9,
+                                stops: [0, 100]
+                            }
+                        }
+                    },
+                    series: [{
+                        data: []
+                    }]
+            }
+        this.getPoints = this.getPoints.bind(this);
     }
 
-    componentDidMount() {
-        axios.get(`https://www.quandl.com/api/v3/datasets/WIKI/${'FB'}.json?start_date=2018-01-01&end_date=2019-01-01&api_key=${API_KEY}`)
-        .then(({data}) => {
-            console.log()
-            this.setState({series: data})
+
+    getPoints() {
+        this.state.data.map((entry) => {
+            console.log([entry[0], entry[1]])
         })
     }
 
 
+    componentDidMount() {
+        axios.get(`https://www.quandl.com/api/v3/datasets/WIKI/${'FB'}.json?start_date=2018-01-01&end_date=2019-01-01&api_key=${API_KEY}`)
+        .then(({ data }) => {
+            console.log(data.dataset.data)
+            console.log(data.dataset.data)
+            this.setState({data: dataset.data})
+        })
+    }
+
+
+
     render() {
+        this.getPoints()
         return (
             <div id="chart">
                 shdjhslkjdk
