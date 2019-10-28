a846f0318ac3ef4a35cff868ed16dd7b460d5df5
kaylaTill <kaylatillman319@gmail.com>
2019-10-27 20:47:22 -0700

    Buy Collapse Working

--- client/src/privatePages/stocks/StockItem.js
+++ client/src/privatePages/stocks/StockItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Collapse, Button } from 'react-bootstrap';
+import { Collapse, Button, Form } from 'react-bootstrap';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './StockList.css';
 
@@ -7,28 +7,70 @@ class StockListItem extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            open: false
+            itemOpen: false,
+            buyMoreOpen: false,
+            sellOpen: false,
+            shares: 0,
+            companyName: '',
+            symbol: '',
+
         }
-        this.collapse = this.collapse.bind(this);
+        this.collapseItem = this.collapseItem.bind(this);
+        this.collapseBuy = this.collapseBuy.bind(this);
+        this.collapseSell = this.collapseSell.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleShareUpdate = this.handleShareUpdate.bind(this);
+        this.handleBuy = this.handleBuy.bind(this);
     }
 
 
-    collapse() {
-        this.setState({ open: !this.state.open })
+    collapseItem() {
+        this.setState({ itemOpen: !this.state.itemOpen })
     }
+    
+    collapseBuy() {
+        this.setState({ buyMoreOpen: !this.state.buyMoreOpen })
+    }
+    
+    collapseSell() {
+        this.setState({ sellOpen: !this.state.sellOpen })
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    handleShareUpdate(event) {
+        event.target.name == 'increase' ? this.setState({ shares: this.state.shares + 1 }) : this.setState({ shares: this.state.shares - 1 })
+    } 
+
+
+    handleBuy(event) {
+        event.preventDefault()
+        this.props.buyStock(
+            this.state.symbol,
+            this.state.companyName,
+            this.state.latestPrice,
+            this.state.shares
+        )
+    }
+
 
 
 
     render() {
         console.log(this.props.stock)
         const { stock } = this.props;
-
         return (
             <>
-                <tr className="stock-item" 
+                <tr 
+                    name="itemOpen"
+                    className="stock-item" 
                     aria-controls="item-info"
-                    aria-expanded={this.state.open}
-                    onClick={this.collapse}
+                    aria-expanded={this.state.itemOpen}
+                    onClick={this.collapseItem}
                 >
                     <td className="company-name">{stock.companyName}</td>
                     <td className="symbol-table">{stock.symbol}</td>
@@ -36,13 +78,47 @@ class StockListItem extends React.Component {
                     <td className="total-shares">{stock.shares}</td>
                     <td className="total-price">{stock.shares * stock.price}</td>
                 </tr>
-                <Collapse in={this.state.open}>
+                <Collapse in={this.state.itemOpen}>
                     <div id="item-info">
                         <Button
                             className="buy-more"
-                            variant="outline-light" size="sm" 
-                            block
+                            variant="outline-light" 
+                            size="sm" block
+                            aria-controls="buy-more-form"
+                            onClick={this.collapseBuy}
+                            aria-expanded={this.state.buyMoreOpen}
                         > Buy More Shares</Button>
+
+                        <Collapse in={this.state.buyMoreOpen}>
+                            <Form id="buy-more-form">
+                                <Button variant="outline-light"
+                                    size="sm"
+                                    name="decrease"
+                                    className="decrease"
+                                    onClick={this.handleShareUpdate}>
+                                    -</Button>
+                                <input
+                                    type="number"
+                                    className="share-quantity"
+                                    name="shares"
+                                    min={0}
+                                    max={100}
+                                    value={this.state.shares > 0 && this.state.shares}
+                                    onChange={this.handleChange}
+                                />
+                                <Button variant="outline-light"
+                                    size="sm" name="increase"
+                                    className="increase"
+                                    onClick={this.handleShareUpdate} >
+                                    +</Button>
+                            <Button
+                                type="submit"
+                                className="submit-buy"
+                                variant="outline-light"
+                                size="sm" block
+                                >Buy</Button>
+                            </Form>
+                        </Collapse>
                         
                         <Button
                             className="sell"
