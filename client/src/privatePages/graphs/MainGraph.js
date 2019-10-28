717c9e5567143fa4145785560dfaa22f7c8e2f59
kaylaTill <kaylatillman319@gmail.com>
2019-10-28 12:23:44 -0700

    Added options to grapgh

--- client/src/privatePages/graphs/MainGraph.js
+++ client/src/privatePages/graphs/MainGraph.js
@@ -1,87 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 const API_KEY = 't7V6zWDxHvWkYYdxXzEN';
+var CanvasJSReact = require('./canvasjs-2.3.2/canvasjs.react');
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-class AreaChart extends React.Component {
+class LineGraph extends Component {
     
     constructor(props) {
         super(props);
         
         this.state = {
-            data: [],
-            selection: 'one_year',
-                options: {
-                    annotations: {
-                        yaxis: [{
-                            y: 30,
-                            borderColor: '#999',
-                        }],
-                        xaxis: [{
-                            x: new Date('14 Nov 2012').getTime(),
-                            borderColor: '#999',
-                            yAxisIndex: 0,
-                        }]
-                    },
-                    dataLabels: {
-                        enabled: false
-                    },
-                    markers: {
-                        size: 0,
-                        style: 'hollow',
-                    },
-                    xaxis: {
-                        type: 'datetime',
-                        min: new Date('01 Mar 2012').getTime(),
-                        tickAmount: 6,
-                    },
-                    tooltip: {
-                        x: {
-                            format: 'dd MMM yyyy'
-                        }
-                    },
-                    fill: {
-                        type: 'gradient',
-                        gradient: {
-                            shadeIntensity: 1,
-                            opacityFrom: 0.7,
-                            opacityTo: 0.9,
-                            stops: [0, 100]
-                        }
-                    }
-                },
-            series: [{
-                data: []
-            }]
+            data: []
+           
         }
-        this.getPoints = this.getPoints.bind(this);
+        // this.getPoints = this.getPoints.bind(this);
     }
 
 
-    getPoints() {
-        this.state.data.map((entry) => {
-            console.log([entry[0], entry[1]])
-        })
-    }
-
-
+    // getPoints() {
+    //     this.state.data.map((entry) => {
+    //         this.state.series[0].data.push([entry[0], entry[1]])
+    //     })
+    // }
+    
+    
     componentDidMount() {
         axios.get(`https://www.quandl.com/api/v3/datasets/WIKI/${'FB'}.json?start_date=2018-01-01&end_date=2019-01-01&api_key=${API_KEY}`)
         .then(({ data }) => {
             this.setState({data: data.dataset.data})
         })
+        .catch((err) => {
+            console.log(err)
+        });
     }
-
-
-
+    
+    
     render() {
-        this.getPoints()
+        this.state.data.map((entry) => {
+            options.data[0].dataPoints.push([entry[0], entry[1]])
+        })
+
+        const options = {
+            animationEnabled: true,
+            exportEnabled: true,
+            theme: "dark2", // "light1", "dark1", "dark2"
+            title: {
+                text: 'Stock'
+            },
+            axisY: {
+                title: "Price (Open)",
+                includeZero: false,
+                prefix: "$"
+            },
+            axisX: {
+                title: "Week of Year",
+                interval: 2
+            },
+            data: [{
+                type: "line",
+                toolTipContent: "Week {x}: {y}%",
+                dataPoints: []
+            }]
+        }
         return (
-            <div id="chart">
-                shdjhslkjdk
+            <div>
+                <CanvasJSChart options={options}
+                /* onRef={ref => this.chart = ref} */
+                />
             </div>
         );
     }
 }
 
 
-export default AreaChart
+
+export default LineGraph;
