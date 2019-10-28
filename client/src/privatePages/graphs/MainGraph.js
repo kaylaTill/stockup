c60aa8297d815653514d6d0c385983eb574cc176
kaylaTill <kaylatillman319@gmail.com>
2019-10-28 11:42:07 -0700

    Data values and points displaying to screen

--- client/src/privatePages/graphs/MainGraph.js
+++ client/src/privatePages/graphs/MainGraph.js
@@ -10,49 +10,49 @@ class AreaChart extends React.Component {
         this.state = {
             data: [],
             selection: 'one_year',
-                    options: {
-                        annotations: {
-                            yaxis: [{
-                                y: 30,
-                                borderColor: '#999',
-                            }],
-                            xaxis: [{
-                                x: new Date('14 Nov 2012').getTime(),
-                                borderColor: '#999',
-                                yAxisIndex: 0,
-                            }]
-                        },
-                        dataLabels: {
-                            enabled: false
-                        },
-                        markers: {
-                            size: 0,
-                            style: 'hollow',
-                        },
-                        xaxis: {
-                            type: 'datetime',
-                            min: new Date('01 Mar 2012').getTime(),
-                            tickAmount: 6,
-                        },
-                        tooltip: {
-                            x: {
-                                format: 'dd MMM yyyy'
-                            }
-                        },
-                        fill: {
-                            type: 'gradient',
-                            gradient: {
-                                shadeIntensity: 1,
-                                opacityFrom: 0.7,
-                                opacityTo: 0.9,
-                                stops: [0, 100]
-                            }
+                options: {
+                    annotations: {
+                        yaxis: [{
+                            y: 30,
+                            borderColor: '#999',
+                        }],
+                        xaxis: [{
+                            x: new Date('14 Nov 2012').getTime(),
+                            borderColor: '#999',
+                            yAxisIndex: 0,
+                        }]
+                    },
+                    dataLabels: {
+                        enabled: false
+                    },
+                    markers: {
+                        size: 0,
+                        style: 'hollow',
+                    },
+                    xaxis: {
+                        type: 'datetime',
+                        min: new Date('01 Mar 2012').getTime(),
+                        tickAmount: 6,
+                    },
+                    tooltip: {
+                        x: {
+                            format: 'dd MMM yyyy'
                         }
                     },
-                    series: [{
-                        data: []
-                    }]
-            }
+                    fill: {
+                        type: 'gradient',
+                        gradient: {
+                            shadeIntensity: 1,
+                            opacityFrom: 0.7,
+                            opacityTo: 0.9,
+                            stops: [0, 100]
+                        }
+                    }
+                },
+            series: [{
+                data: []
+            }]
+        }
         this.getPoints = this.getPoints.bind(this);
     }
 
@@ -67,9 +67,7 @@ class AreaChart extends React.Component {
     componentDidMount() {
         axios.get(`https://www.quandl.com/api/v3/datasets/WIKI/${'FB'}.json?start_date=2018-01-01&end_date=2019-01-01&api_key=${API_KEY}`)
         .then(({ data }) => {
-            console.log(data.dataset.data)
-            console.log(data.dataset.data)
-            this.setState({data: dataset.data})
+            this.setState({data: data.dataset.data})
         })
     }
 
